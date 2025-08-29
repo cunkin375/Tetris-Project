d2da29978fe701f419fdec85304c10f24b93ec65
ali <159713281+cunkin375@users.noreply.github.com>
2025-08-29 15:24:26 -0400

    Add TETRIS_WHITE to ColorHandler

--- src/ColorHandler.h
+++ src/ColorHandler.h
@@ -10,7 +10,8 @@ enum Colors {
     TETRIS_YELLOW,
     TETRIS_PURPLE,
     TETRIS_CYAN,
-    TETRIS_BLUE
+    TETRIS_BLUE,
+    TETRIS_WHITE
 };
 
 namespace ColorHandler {
@@ -25,7 +26,8 @@ namespace ColorHandler {
         case TETRIS_PURPLE:    return {160,  32, 240, 255};
         case TETRIS_CYAN:      return {  0, 255, 255, 255};
         case TETRIS_BLUE:      return {  0,   0, 255, 255};
-        default: /* White */   return {255, 255, 255, 255};
+        case TETRIS_WHITE:     return {255, 255, 255, 255};
+        default:/*Just Incase*/return {255, 255, 255, 255};
         } 
     } 
 }
