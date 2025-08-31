8e55407d15238a7269cc09163aceb0e2d99d0ffe
ali <159713281+cunkin375@users.noreply.github.com>
2025-08-30 21:00:21 -0400

    Add reminder comment in Constants

--- src/Constants.h
+++ src/Constants.h
@@ -8,12 +8,14 @@ constexpr size_t g_CellSplit = 1;
 constexpr size_t g_PositionCount = 4;
 
 // Window Setttings
+// Move to Window class
 constexpr uint32_t InitWindowWidth = 500;
 constexpr uint32_t InitWindowHeight = 620;
 constexpr uint32_t TargetFPS = 144;
 constexpr double_t EventWindowMS = 0.5;
 
 // Game Over Message 
+// Move to window class
 constexpr uint32_t xPosition = 40;  
 constexpr uint32_t yPosition = 280; 
 constexpr uint32_t FontSize = 20;   
