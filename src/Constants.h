5e74e436b858ac2dae2c0df4f5b386cda97ebd8f
ali <159713281+cunkin375@users.noreply.github.com>
2025-08-30 20:36:16 -0400

    Refactir main variables

--- src/Constants.h
+++ src/Constants.h
@@ -6,3 +6,14 @@ constexpr size_t g_CellSize = 30;
 constexpr size_t g_CellOffset = 11;
 constexpr size_t g_CellSplit = 1;
 constexpr size_t g_PositionCount = 4;
+
+// Window Setttings
+constexpr uint32_t InitWindowWidth = 500;
+constexpr uint32_t InitWindowHeight = 620;
+constexpr uint32_t TargetFPS = 144;
+constexpr double_t EventWindowMS = 0.5;
+
+// Game Over Message 
+constexpr uint32_t xPosition = 40;  
+constexpr uint32_t yPosition = 280; 
+constexpr uint32_t FontSize = 20;   
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,6 @@
 
 int main() {
     // Remember to refactor magic numbers 
-    uint32_t InitWindowWidth = 500;
-    uint32_t InitWindowHeight = 620;
-    uint32_t TargetFPS = 144;
     InitWindow(InitWindowWidth, InitWindowHeight, "Raylib Tetris"); 
     SetTargetFPS(TargetFPS);
     Color BackgroundColor = ColorHandler::Get(TETRIS_DARK_BLUE);
@@ -16,12 +13,9 @@ int main() {
         BeginDrawing();
         if (!Game.IsOver() && !Game.IsPaused()) {
             Game.HandleInput();
-            if (Game.EventTriggered(0.5)) Game.MoveBlockDown();
+            if (Game.EventTriggered(EventWindowMS)) Game.MoveBlockDown();
             Game.Render();
         } else {
-            uint32_t xPosition = 40;
-            uint32_t yPosition = 280;
-            uint32_t FontSize = 20;
             DrawText("Game Over!\n Press ENTER to Restart", xPosition, yPosition, FontSize, ColorHandler::Get(TETRIS_WHITE));
             if (IsKeyPressed(KEY_ENTER)) Game.ResetGame();
         }
