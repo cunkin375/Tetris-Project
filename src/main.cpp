43a2faa77baaa4c17af0dd9878029882e24fc287
ali <159713281+cunkin375@users.noreply.github.com>
2025-08-30 20:26:14 -0400

    Refactor main.cpp variables

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,12 @@
 #include <raylib.h>
 
 int main() {
-    InitWindow(500, 620, "Raylib Tetris"); 
-    SetTargetFPS(144);
+    // Remember to refactor magic numbers 
+    uint32_t InitWindowWidth = 500;
+    uint32_t InitWindowHeight = 620;
+    uint32_t TargetFPS = 144;
+    InitWindow(InitWindowWidth, InitWindowHeight, "Raylib Tetris"); 
+    SetTargetFPS(TargetFPS);
     Color BackgroundColor = ColorHandler::Get(TETRIS_DARK_BLUE);
     TetrisGame Game; 
     while (!WindowShouldClose()) {
@@ -13,9 +17,12 @@ int main() {
         if (!Game.IsOver() && !Game.IsPaused()) {
             Game.HandleInput();
             if (Game.EventTriggered(0.5)) Game.MoveBlockDown();
-            Game.Update();
+            Game.Render();
         } else {
-            DrawText("Game Over!\n Press ENTER to Restart", 20, 280, 20, ColorHandler::Get(TETRIS_WHITE));
+            uint32_t xPosition = 40;
+            uint32_t yPosition = 280;
+            uint32_t FontSize = 20;
+            DrawText("Game Over!\n Press ENTER to Restart", xPosition, yPosition, FontSize, ColorHandler::Get(TETRIS_WHITE));
             if (IsKeyPressed(KEY_ENTER)) Game.ResetGame();
         }
         ClearBackground(BackgroundColor);
